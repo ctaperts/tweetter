12d6efff2d73007c32f49051571dff20eb6fdb77
Colby Taperts <colbytaperts@gmail.com>
2016-08-23 10:40:47 -0500

    add validaitons for twits and username;

--- app/models/twit.rb
+++ app/models/twit.rb
@@ -1,3 +1,11 @@
 class Twit < ApplicationRecord
   belongs_to :user
+
+  validates :message, 
+	    presence: true,
+	    length: { 
+		  maximum: 140,
+		  too_long: "Twits can only be 140 characters max" 
+		  }
+
 end
