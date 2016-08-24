1feeaa7c65f3c301f395bb6f92a50b726ba49b13
Colby Taperts <colbytaperts@gmail.com>
2016-08-24 08:34:12 -0500

    setup relationship and basic relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :twits
+  has_many :relationships
+  has_many :friends, through: :relationships
 
   validates :username,
 	    presence: true,
